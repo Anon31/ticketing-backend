459d06e450bf5c58faf92da6b36f6e4bc4badecc
Anon31 <anon31@protonmail.com>
2026-04-16 00:50:49 +0200

    Mise à jour du jwt.strategy.ts

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
 export interface JwtPayload {
     sub: string;
