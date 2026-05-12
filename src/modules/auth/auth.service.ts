efdd4629c4706f8d9284bc9a59446626399a43b1
Anon31 <anon31@protonmail.com>
2026-05-12 14:04:52 +0200

    feat(develop): test sonar

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -37,5 +37,6 @@ export class AuthService {
         // Délégation au UsersService selon le principe SRP
         const user = await this.usersService.create(registerDto);
         return this.login({ email: user.email, password: registerDto.password });
+        console.log("test");
     }
 }
